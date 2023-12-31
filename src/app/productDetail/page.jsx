a3071a6a490df84aed2fed145febf6d1f52aeb25
sekune <ahlatlipost@gmail.com>
2023-12-31 11:57:25 +0300

    Fix responsive

--- src/app/productDetail/page.jsx
+++ src/app/productDetail/page.jsx
@@ -1,48 +1,4 @@
-"use client"; // This is a client component 👈🏽
-// import React, { useState } from "react";
-// import { data } from "../../services/data";
-// import ProductSlider from "../components/ProductSlider";
-// import ProSelectAtt from "./ProSelectAtt";
-// import ProSelectBarem from "./ProSelectBarem";
-// import { Toaster } from "react-hot-toast";
-
-// const ProductDetail = () => {
-//   //! State where the data added to the cart is stored
-//   const [addToCart, setAddToCart] = useState({
-//     id: "",
-//     size: "",
-//     amount: "",
-//     color: "",
-//     price: "",
-//     product: "",
-//     totalPrice: "",
-//   });
-
-//   return (
-//     <main className="grid grid-cols-2 min-h-screen items-center">
-//       <Toaster toastOptions={{ duration: 3000 }} />
-//       {/* The 0th element of productVariants is considered selected. */}
-//       <ProductSlider images={data.productVariants[0].images} />
-//       <div className="ml-2">
-//         <ProSelectAtt
-//           title={data.productTitle}
-//           info={data.selectableAttributes}
-//           baremList={data.baremList}
-//           setAddToCart={setAddToCart}
-//         />
-//         <ProSelectBarem
-//           info={data.baremList}
-//           productVariant={data.productVariants[0]}
-//           setAddToCart={setAddToCart}
-//           addToCart={addToCart}
-//         />
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default ProductDetail;
-
+"use client";
 import React, { useState } from "react";
 import { data } from "../../services/data";
 import ProductSlider from "../components/ProductSlider";
@@ -62,7 +18,7 @@ const ProductDetail = () => {
   });
 
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 sm:mt-3 min-h-screen">
+    <main className="grid grid-cols-1 md:grid-cols-2 mt-2 min-h-screen">
       <Toaster toastOptions={{ duration: 3000 }} />
       <div className="order-1 md:order-1">
         <ProductSlider images={data.productVariants[0].images} />
